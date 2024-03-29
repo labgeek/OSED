66841584c2a40b6eb4430f228826534920eaf252
JD <labgeek@users.noreply.github.com>
2024-03-29 17:08:35 -0400

    Update installmona.sh

--- installmona.sh
+++ installmona.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 TOOLS=("https://github.com/corelan/windbglib/raw/master/pykd/pykd.zip" "https://github.com/corelan/windbglib/raw/master/windbglib.py" "https://github.com/corelan/mona/raw/master/mona.py" "https://www.python.org/ftp/python/2.7.17/python-2.7.17.msi" "https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe" "https://raw.githubusercontent.com/labgeek/OSED/main/installmona.ps1")
 
 TMPDIR=$(mktemp -d)
