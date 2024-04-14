f56e9cf27f5edf661743ffe33757c3e8bd2f9565
JD <labgeek@users.noreply.github.com>
2024-04-14 09:07:48 -0400

    Update installmona.ps1
    added customized windbg.reg file

--- tools/installmona.ps1
+++ tools/installmona.ps1
@@ -15,12 +15,19 @@ echo "[+] backing up old pykd files"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd.bak"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll.bak"
 
-# install python2.7
+# Installing personalized windbg workspace for labgeek
+echo "[+] Installing the WINDBG workspace for labgeek"
+copy "$share_path\windbg.reg" $install_dir
+cd $install_dir
+reg import .\windbg.reg
 
+
+# install python2.7
 echo "[+] installing python2.7"
 copy "$share_path\python-2.7.17.msi" $install_dir
 msiexec.exe /i $install_dir\python-2.7.17.msi /qn
 start-sleep 60
+
 
 # register Python2.7 binaries in path before Python3
 echo "[+] adding python2.7 to the PATH"
@@ -36,7 +43,7 @@ copy "$share_path\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\wine
 # register runtime debug dll
 echo "[+] registering runtime debug dll"
 cd "C:\Program Files\Common Files\Microsoft Shared\VC"
-regsvr32 /s msdia90.dll
+
 
 echo "[=] in case you see something about symbols when running mona, try executing the following (the runtime took too long to install)"
 echo "regsvr32 C:\Program Files\Common Files\Microsoft Shared\VC\msdia90.dll"
